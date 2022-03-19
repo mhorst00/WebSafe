5aab75f1ac54aa7663cb0c58f2485b31c592223c
malte.horst <maltehorst@gmail.com>
2022-03-19 16:37:13 +0100

    added mail greeting on registration

--- backend/app/mail.py
+++ backend/app/mail.py
@@ -11,7 +11,7 @@ SMTP_SERVER = config("SMTP_SERVER")
 SMTP_PORT = config("SMTP_PORT", cast=int)
 MAIL_ADDRESS = config("MAIL_ADDRESS")
 MAIL_PASSWORD = config("MAIL_PASSWORD")
-URI = config("HOST_URI", default="localhost")
+URI = config("HOST_URI", default="localhost:8000")
 
 class MailSend():
     CLIENT = None
@@ -32,4 +32,15 @@ class MailSend():
         msg.set_content(f"""Hello {user.full_name}, \n
         you have requested to delete your account. This may be because you have forgotten your password. \n
         Click this link to confirm your account deletion: https://{URI}/api/v1/user/delete/verify/{del_string}""")
+        MailSend.CLIENT.send_message(msg)
+
+    @staticmethod
+    def send_user_greeting(user: UserInDB):
+        msg = EmailMessage()
+        msg["Subject"] = f"WebSafe - Creation of your account ({user.full_name})"
+        msg["From"] = f"noreply@{URI}"
+        msg["To"] = user.username
+        msg.set_content(f"""Hello {user.full_name}, \n
+        your account creation has been successful. \n
+        Click this link to login to your new account: https://{URI}/login""")
         MailSend.CLIENT.send_message(msg)
