c73cb636970a37cc40f3c9928a257aae5f6e95a3
malte.horst <maltehorst@gmail.com>
2022-03-19 16:25:42 +0100

    added mail backend and modified other files accordingly

--- backend/app/user.py
+++ backend/app/user.py
@@ -1,8 +1,9 @@
 import hashlib
-
+import random
+import string
 from pydantic import validate_arguments
 
-from app.model import User, UserInDB, UserNew
+from app.model import User, UserInDB, UserInDBDel,UserNew
 from app.db import Database
 from app.auth import get_password_hash
 from app.files import Filehandler
@@ -29,10 +30,7 @@ def get_user(user: User):
 
 @validate_arguments
 def delete_user(user: UserInDB):
-    """Deletes user from DB and deltes their Safe. 
-    Returns True on success, False if user is not found or if the Safe could'nt be deleted"""
-    if not Database.find_user(user):
-        return False
+    """Deletes user from DB. Returns True on success."""
     x = Database.delete_user(user)
     if x:
         y =  Filehandler.deleteFile(user.safe_id)
@@ -43,15 +41,37 @@ def delete_user(user: UserInDB):
 @validate_arguments
 def edit_user(data: UserNew, user: UserInDB):
     """Saves received data in given user in DB"""
-    x = Database.find_user(user)
-    if not x:
-        return False # add logging for failures on if's
     d = Database.delete_user(user)
     if not d:
         return False
     hashed_password = get_password_hash(data.password)
     # TODO: use hash of username as safe_id
-    user_dict = UserInDB(username=data.username, full_name=data.full_name, hashed_password=hashed_password, safe_id=x.safe_id)
+    user_dict = UserInDB(username=data.username, full_name=data.full_name, hashed_password=hashed_password, safe_id=user.safe_id)
     Database.add_user(user_dict)
     if Database.find_user(user_dict) is not None:
         return True
+
+@validate_arguments
+def add_deletion_string(user: UserInDB):
+    """Adds verification string to user in DB. Returns string on success."""
+    del_string = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(32))
+    user_dict = UserInDBDel(
+        username=user.username,
+        full_name=user.full_name,
+        hashed_password=user.hashed_password,
+        safe_id=user.safe_id,
+        del_string=del_string
+    )
+    Database.delete_user(user)
+    x = Database.add_user(user_dict)
+    if x is not None:
+        return del_string
+
+@validate_arguments
+def find_deletion_string(del_string: str) -> UserInDBDel | None:
+    """Searches DB for user with given deletion string"""
+    if len(del_string) != 32:
+        return None
+    user = Database.find_by_value("del_string", del_string)
+    if user.del_string:
+        return user
