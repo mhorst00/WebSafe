7ceae88b04332346e0a2a66b83cc701c5a7e9c9d
Cyber-Luke <lukas-ernst@outlook.com>
2022-03-29 12:19:09 +0200

    login fix

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,16 +1,16 @@
 import { createContext, useState } from "react";
 
 export const AuthContext = createContext();
-export const AuthProvider = ({children}) => {
+export const AuthProvider = ({ children }) => {
     const [authState, setAuthState] = useState(undefined);
 
-    const login = () => {
-        setAuthState('jwt-token')
+    const login = (token) => {
+        setAuthState('jwt-token' + token)
     }
 
     const logout = () => {
         setAuthState(undefined);
     }
 
-    return <AuthContext.Provider value={{authState, login, logout}}>{children}</AuthContext.Provider>;
+    return <AuthContext.Provider value={{ authState, login, logout }}>{children}</AuthContext.Provider>;
 } 
