f38c766f38df48195a1666fd7bb76d070a025d65
Cyber-Luke <lukas-ernst@outlook.com>
2022-03-29 12:19:09 +0200

    login fix

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,7 +1,7 @@
 import { useState, useContext } from 'react';
 import './Login.css';
 import { AuthContext } from '../context/AuthContext';
- 
+
 function Login() {
   const [register, setRegister] = useState(false);
   const [failed, setFailed] = useState(undefined);
@@ -11,11 +11,11 @@ function Login() {
   const [passwordConfirm, setPasswordConfirm] = useState('');
 
 
-  const {login} = useContext(AuthContext);
-  
+  const { login } = useContext(AuthContext);
+
   const onClickLogin = (event) => {
-      event.preventDefault();
-      setRegister(false);
+    event.preventDefault();
+    setRegister(false);
   }
 
   const onClickRegister = (event) => {
@@ -41,7 +41,7 @@ function Login() {
 
   const validateInput = () => {
     let matchEmail = /\S+@\S+\.\S+/;
-    if(!register) {
+    if (!register) {
       return matchEmail.test(email) && password.length > 6;
     }
     return matchEmail.test(email) && password.length > 6 && password === passwordConfirm;
@@ -53,14 +53,15 @@ function Login() {
       setFailed('There was a problem with your E-Mail!');
       return;
     }
-    */ 
+    */
 
     try {
-      if(register) {
+      if (register) {
         console.log('API-CALL {' + name + email + password + passwordConfirm + '}');
       } else {
         console.log('API-CALL {' + email + password + '}');
-        login();
+        let token = 'token';
+        login(token);
       }
     } catch (err) {
       setFailed('Wrong Password or E-Mail!');
@@ -70,34 +71,34 @@ function Login() {
   }
 
   return (
-    <div className='Login-Container'> 
-      <div className="Login-Field"> 
+    <div className='Login-Container'>
+      <div className="Login-Field">
         <h2>WebSafe</h2>
         {failed ? (<p className='Login-Failed-Text'>{failed}</p>) : (<p></p>)}
         <div className='Login-Input'>
-            {register && (
-                <>
-                    <label className='Login-Password'>Name</label>
-                    <input type='password' placeholder='Your Name' onChange={onChangeName}/>
-                </>
-            )}
-            <label className='Login-Password'>E-Mail</label>
-            <input type='email' placeholder='example@example.com' onChange={onChangeEmail}/>
-            <label className='Login-Password'>Password</label>
-            <input type='password' placeholder='password' onChange={onChangePassword}/>
-            {register && (
-                <>
-                    <label className='Login-Password'>Password confirm</label>
-                    <input type='password' placeholder='password' onChange={onChangePasswordConfirm}/>
-                </>
-            )}
+          {register && (
+            <>
+              <label className='Login-Password'>Name</label>
+              <input type='password' placeholder='Your Name' onChange={onChangeName} />
+            </>
+          )}
+          <label className='Login-Password'>E-Mail</label>
+          <input type='email' placeholder='example@example.com' onChange={onChangeEmail} />
+          <label className='Login-Password'>Password</label>
+          <input type='password' placeholder='password' onChange={onChangePassword} />
+          {register && (
+            <>
+              <label className='Login-Password'>Password confirm</label>
+              <input type='password' placeholder='password' onChange={onChangePasswordConfirm} />
+            </>
+          )}
 
-            <button className='Login-Button' onClick={onSubmit}>{register? "Register" : "Login"}</button>
-            {register ? (
-                <p className='Login-Info-Text'>You already have an Account? <a href='' onClick={onClickLogin}>Login</a></p>
-            ) : (
-                <p className='Login-Info-Text'>You don't have an Account? <a href='' onClick={onClickRegister}>Register</a></p>
-            )}
+          <button className='Login-Button' onClick={onSubmit}>{register ? "Register" : "Login"}</button>
+          {register ? (
+            <p className='Login-Info-Text'>You already have an Account? <a href='' onClick={onClickLogin}>Login</a></p>
+          ) : (
+            <p className='Login-Info-Text'>You don't have an Account? <a href='' onClick={onClickRegister}>Register</a></p>
+          )}
         </div>
       </div>
     </div>
