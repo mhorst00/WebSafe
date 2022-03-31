2a9afd46439f119e46724a27a333eb991c649e0e
Yannis Plaschko <yannis.plaschko@breuninger.de>
2022-03-31 20:43:41 +0200

    First working api request
    WIP added login api request
    
    WIP added get Safe and delete user calls
    
    First working api request
    
    WIP added login api request
    
    Thats one huge pile of shit
    
    Revert "Thats one huge pile of shit"
    
    This reverts commit db8c34cb6c8a70eb25c8540d027d88643523cdd4.
    
    Revert "Revert "Thats one huge pile of shit""
    
    This reverts commit 3f9ab7285994898f98229f03b46dd1e5c2e6a159.
    
    wip send safe on logout
    
    added noisy failing
    
    wip send safe on logout
    
    flaky send safe on logout
    
    example useEffect
    
    added useeffects for import/export
    
    Rebase stuff
    
    broken stuff
    
    Safe now gets imported after login - caution: still flaky!

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,42 +1,171 @@
-import { useState, useContext } from 'react';
-import './Login.css';
-import { AuthContext } from '../context/AuthContext';
+import { useState, useContext } from "react";
+import "./Login.css";
+import { AuthContext } from "../context/AuthContext";
+import { encryptionModule } from "../encryption";
+
+const baseUrl = "http://websafe.localhost/api/v1";
+
+function isSafeImportable(payload) {
+  if (payload == "") {
+    return false;
+  }
+  if (payload.safe_payload == "") {
+    return false;
+  }
+  if (payload.safe_payload == "") {
+    return false;
+  }
+  if (payload.enc_data_key == "") {
+    return false;
+  }
+  if (payload.enc_vault_key == "") {
+    return false;
+  }
+  if (payload.data_iv == "") {
+    return false;
+  }
+  if (payload.vault_iv == "") {
+    return false;
+  }
+  return true;
+}
+
+function registerUser(email, name, password) {
+  return new Promise(function (resolve, reject) {
+    let request = new XMLHttpRequest();
+    request.open("POST", baseUrl + "/user/new");
+    request.setRequestHeader("Content-Type", "application/json");
+    request.setRequestHeader("Accept", "application/json");
+
+    request.onload = function () {
+      if (request.status === 200) {
+        resolve(request.status);
+      } else {
+        window.alert("Error with call:" + request.responseText);
+        console.log("Error with call:" + request.responseText);
+        reject(request.status);
+      }
+    };
+    request.onerror = function () {
+      window.alert("Error with call:" + request.responseText);
+      console.log("Error with call:" + request.responseText);
+      reject(request.status);
+    };
+    request.send(
+      JSON.stringify({
+        username: email,
+        full_name: name,
+        password: password,
+      })
+    );
+  });
+}
+
+function loginUser(email, password) {
+  return new Promise(function (resolve, reject) {
+    let request = new XMLHttpRequest();
+    request.open("POST", baseUrl + "/token");
+    request.setRequestHeader(
+      "Content-Type",
+      "application/x-www-form-urlencoded"
+    );
+    request.setRequestHeader("Accept", "application/json");
+    request.onload = function () {
+      if (request.status == 200) {
+        resolve(JSON.parse(request.response).access_token);
+      } else {
+        window.alert("Error with call:" + request.responseText);
+        console.log("Error with call:" + request.responseText);
+        reject(request.status);
+      }
+    };
+    request.onerror = function () {
+      window.alert("Error with call:" + request.responseText);
+      console.log("Error with call:" + request.responseText);
+      reject(request.status);
+    };
+    request.send(
+      "grant_type=&username=" +
+        email +
+        "&password=" +
+        password +
+        "&scope=&client_id=&client_secret="
+    );
+  });
+}
+
+export async function getSafe(token, email, password) {
+  return new Promise(function (resolve, reject) {
+    let request = new XMLHttpRequest();
+    request.open("GET", baseUrl + "/safe");
+    request.setRequestHeader("Accept", "application/json");
+    request.setRequestHeader("Authorization", "Bearer " + token);
+
+    request.onload = function () {
+      if (request.status == 200) {
+        //sollte als JSON objekt übergeben
+        let safe = JSON.parse(request.responseText);
+        if (isSafeImportable(safe)) {
+          let decryptedSafe = encryptionModule.importSafe(safe);
+          resolve(decryptedSafe);
+        } else {
+          console.log(
+            "safe was not importable, trying to log in otherwise: ",
+            email,
+            password
+          );
+          encryptionModule.initialise(email, password);
+          request.send();
+        }
+      } else {
+        window.alert("Error with call:" + request.responseText);
+        console.log("Error with call:" + request.responseText);
+      }
+    };
+    request.onerror = function () {
+      window.alert("Error with call:" + request.responseText);
+      console.log("Error with call:" + request.responseText);
+      reject(request.status);
+    };
+    request.send();
+  });
+}
 
 function Login() {
   const [register, setRegister] = useState(false);
   const [failed, setFailed] = useState(undefined);
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("a@b.com");
+  const [password, setPassword] = useState("test");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
 
   const { login } = useContext(AuthContext);
 
   const onClickLogin = (event) => {
     event.preventDefault();
     setRegister(false);
-  }
+  };
 
   const onClickRegister = (event) => {
     event.preventDefault();
     setRegister(true);
-  }
+  };
 
   const onChangeName = (event) => {
     setName(event.target.value);
-  }
+  };
 
   const onChangeEmail = (event) => {
     setEmail(event.target.value);
-  }
+  };
 
   const onChangePassword = (event) => {
     setPassword(event.target.value);
-  }
+  };
 
   const onChangePasswordConfirm = (event) => {
     setPasswordConfirm(event.target.value);
-  }
+  };
 
   const validateInput = () => {
     let matchEmail = /\S+@\S+\.\S+/;
@@ -44,8 +173,12 @@ function Login() {
     if (!register) {
       return matchEmail.test(email) && password.length > 6;
     }
-    return matchEmail.test(email) && password.length > 6 && password === passwordConfirm;
-  }
+    return (
+      matchEmail.test(email) &&
+      password.length > 6 &&
+      password === passwordConfirm
+    );
+  };
 
   const onSubmit = () => {
     /*
@@ -57,48 +190,99 @@ function Login() {
 
     try {
       if (register) {
-        console.log('API-CALL {' + name + email + password + passwordConfirm + '}');
+        // User tries to register
+        registerUser(email, name, password).then((stat) => {
+          if (stat === 200) {
+            encryptionModule.initialise(email, password);
+            loginUser(email, password).then((token) => {
+              if (token.length == 3) {
+                console.log("tolle fehlerbehandlung kommt noch");
+              } else {
+                login(token);
+              }
+            });
+          } else {
+            console.log("tolle fehlerbehandlung kommt noch");
+          }
+        });
       } else {
-        console.log('API-CALL {' + email + password + '}');
-        let token = 'token';
-        login(token);
+        // User tries to log in
+        loginUser(email, password).then(async (token) => {
+          if (token.length == 3) {
+            console.log("tolle fehlerbehandlung kommt noch");
+          } else {
+            await encryptionModule.initialise(email, password);
+            login(token, email, password);
+          }
+        });
       }
     } catch (err) {
-      setFailed('Wrong Password or E-Mail!');
+      setFailed("Wrong Password or E-Mail!");
     }
-    setEmail('');
-    setPassword('');
-  }
+    setEmail("");
+    setPassword("");
+  };
 
   return (
-    <div className='Login-Container'> 
-      <div className="Login-Field"> 
+    <div className="Login-Container">
+      <div className="Login-Field">
         <h2>WebSafe</h2>
-        {failed ? (<p className='Login-Failed-Text'>{failed}</p>) : (<p></p>)}
-        <div className='Login-Input'>
-            {register && (
-                <>
-                    <label className='Login-Password'>Name</label>
-                    <input type='text' placeholder='Your Name' onChange={onChangeName}/>
-                </>
-            )}
-            <label className='Login-Password'>E-Mail</label>
-            <input type='email' placeholder='example@example.com' onChange={onChangeEmail}/>
-            <label className='Login-Password'>Password</label>
-            <input type='password' placeholder='password' onChange={onChangePassword}/>
-            {register && (
-                <>
-                    <label className='Login-Password'>Password confirm</label>
-                    <input type='password' placeholder='password' onChange={onChangePasswordConfirm}/>
-                </>
-            )}
+        {failed ? <p className="Login-Failed-Text">{failed}</p> : <p></p>}
+        <div className="Login-Input">
+          {register && (
+            <>
+              <label className="Login-Password">Name</label>
+              <input
+                type="text"
+                placeholder="Your Name"
+                onChange={onChangeName}
+              />
+            </>
+          )}
+          <label className="Login-Password">E-Mail</label>
+          <input
+            type="email"
+            placeholder="example@example.com"
+            onChange={onChangeEmail}
+          />
+          <label className="Login-Password">Password</label>
+          <input
+            type="password"
+            placeholder="password"
+            onChange={onChangePassword}
+          />
+          {register && (
+            <>
+              <label className="Login-Password">Password confirm</label>
+              <input
+                type="password"
+                placeholder="password"
+                onChange={onChangePasswordConfirm}
+              />
+            </>
+          )}
 
-            <button className='Login-Button' onClick={onSubmit}>{register? "Register" : "Login"}</button>
-            {register ? (
-                <p className='Login-Info-Text'>You already have an Account? <a href='https://gruppe4.testsites.info/' onClick={onClickLogin}>Login</a></p>
-            ) : (
-                <p className='Login-Info-Text'>You don't have an Account? <a href='https://gruppe4.testsites.info/' onClick={onClickRegister}>Register</a></p>
-            )}
+          <button className="Login-Button" onClick={onSubmit}>
+            {register ? "Register" : "Login"}
+          </button>
+          {register ? (
+            <p className="Login-Info-Text">
+              You already have an Account?{" "}
+              <a href="https://gruppe4.testsites.info/" onClick={onClickLogin}>
+                Login
+              </a>
+            </p>
+          ) : (
+            <p className="Login-Info-Text">
+              You don't have an Account?{" "}
+              <a
+                href="https://gruppe4.testsites.info/"
+                onClick={onClickRegister}
+              >
+                Register
+              </a>
+            </p>
+          )}
         </div>
       </div>
     </div>
--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -3,21 +3,40 @@ import { createContext, useState } from "react";
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [authState, setAuthState] = useState(undefined);
-    const [email, setEmail] = useState(undefined);
-    const [password, setPassword] = useState(undefined);
+  const [authState, setAuthState] = useState(undefined);
+  const [email, setEmail] = useState(undefined);
+  const [password, setPassword] = useState(undefined);
 
-    const login = (token, email, password) => {
-        setAuthState('jwt-token ' + token);
-        setEmail('email ' + email);
-        setPassword('password ' + password);
-    }
+  const [userEmail, setUserEmail] = useState(undefined);
+  const [userPassword, setUserPassword] = useState(undefined);
 
-    const logout = () => {
-        setAuthState(undefined);
-        setEmail(undefined);
-        setPassword(undefined);
-    }
+  const login = (token, email, password) => {
+    setAuthState(token);
+    setEmail(email);
+    setUserEmail(email);
+    setPassword(password);
+    setUserPassword(password);
+  };
 
-    return <AuthContext.Provider value={{ authState, login, logout }}>{children}</AuthContext.Provider>;
-} 
+  const logout = () => {
+    setAuthState(undefined);
+    setEmail(undefined);
+    setPassword(undefined);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        authState,
+        email,
+        password,
+        userEmail,
+        userPassword,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
