a2ac90f2b977cd5e3819be8fa9ad83198012f103
malte.horst <malte.horst@hpe.com>
2022-05-10 20:51:26 +0200

    Added license notes to files

--- backend/app/mail.py
+++ backend/app/mail.py
@@ -1,3 +1,20 @@
+"""
+This file is part of WebSafe and has been contributed by https://github.com/mhorst00.
+
+WebSafe is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import logging
 import smtplib
 
@@ -26,11 +43,12 @@ class MailSend:
         if MAIL_UNSET:
             return None
         MailSend.CLIENT = smtplib.SMTP_SSL(host=SMTP_SERVER, port=SMTP_PORT)
-        MailSend.CLIENT.ehlo()
+        MailSend.CLIENT.ehlo()  # tests the connection to the SMTP server
         MailSend.CLIENT.login(user=MAIL_ADDRESS, password=MAIL_PASSWORD)
 
     @staticmethod
     def send_user_delete(user: UserInDB):
+        """Adds a string for deletion verification to the user and sends it via email"""
         if MAIL_UNSET:
             return None
         del_string = u.add_deletion_string(user)
@@ -51,6 +69,7 @@ class MailSend:
 
     @staticmethod
     def send_user_greeting(user: UserInDB):
+        """Greets a new user via email"""
         if MAIL_UNSET:
             return None
         msg = EmailMessage()
